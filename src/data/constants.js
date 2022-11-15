501abdf97c6b77499045b3662bc3be45b5c8cfc5
Ahmad-045 <ahmad.amin@devsinc.com>
2022-11-15 19:20:03 +0500

    Changed API url origin

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,5 +1,5 @@
-export const BASE_URL = 'http://localhost:3000';
-// export const BASE_URL = 'https://glacial-ravine-73785.herokuapp.com';
+// export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://glacial-ravine-73785.herokuapp.com';
 
 export const messages = {
   incorrect_credentails: 'Incorrect Credentials',
